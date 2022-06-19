7b35927b36e46d56347ae294e2cf110a223839c6
ditcu-code <work.adityac@gmail.com>
2022-06-20 00:12:02 +0700

    ditcu - connect card

--- Hover/views/screen/home/HomeScreen.swift
+++ Hover/views/screen/home/HomeScreen.swift
@@ -9,9 +9,35 @@ import SwiftUI
 
 struct HomeScreen: View {
     var body: some View {
-        VStack {
-            Text("Home Screen")
-            Tag()
+        
+        NavigationView {
+            List {
+                NavigationLink(destination: Tag()) {
+                    HStack(spacing: 0) {
+                        HStack {
+                            Image("woman").resizable().frame(width: 60, height: 60).clipShape(Circle())
+                            Image("man").resizable().frame(width: 60, height: 60).clipShape(Circle()).offset(x: -20, y: 0).zIndex(-1)
+                        }
+                        HStack {
+                            Text("Jeniver").fontWeight(.bold)
+                            Image(systemName: "app.connected.to.app.below.fill")
+                            Text("Adit").fontWeight(.bold)
+                        }.font(.title2)
+                    }
+                }
+            }
+            
+//            .navigationBarHidden(true)
+            .navigationTitle("Hover")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: Text("Plus")) {
+                        Image(systemName: "plus.circle.fill").symbolRenderingMode(.palette)
+                            .foregroundStyle(.black, .yellow)
+                    }
+                }
+            }
         }
     }
 }
