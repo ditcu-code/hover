173063e090f662cfb4a23e523fffe4bab90381c6
ditcu-code <work.adityac@gmail.com>
2022-06-19 20:55:57 +0700

    Initial Commit

---
+++ Hover/HoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoverApp.swift
+//  Hover
+//
+//  Created by Aditya Cahyo on 19/06/22.
+//
+
+import SwiftUI
+
+@main
+struct HoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
