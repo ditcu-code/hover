ed41f13449d82978404d4a28d13f1929dc1bbe20
m-aleandroo13 <77022709+m-aleandroo13@users.noreply.github.com>
2022-06-27 20:11:56 +0800

    Feature/core data model test write and read (#3)
    Add Core data capability

--- Hover/HoverApp.swift
+++ Hover/HoverApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct HoverApp: App {
+    @StateObject private var dataController = DataController()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext )
         }
     }
 }
