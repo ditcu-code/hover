9c9d43c65fbfab10a25866e37beb1c5f629d0827
Eddo Careera Iriyanto Putra <eddocareera.i.p@gmail.com>
2022-06-29 17:32:02 +0700

    Task/user prompt (#4)
    create view prompt name and special date

---
+++ Hover/views/_shared/OnboardingNextButton.swift
@@ -0,0 +1,31 @@
+//
+//  OnboardingNextButton.swift
+//  Hover
+//
+//  Created by Eddo Careera Iriyanto Putra on 28/06/22.
+//
+
+import SwiftUI
+
+struct OnboardingNextButton: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .frame(width: 90, height: 90)
+                .foregroundColor(Color("PrimaryColor"))
+            Image(systemName: "chevron.right")
+                .font(.title)
+                .padding(10)
+                .frame(width: 37, height: 37)
+                .foregroundColor(Color("PrimaryColor"))
+                .background(Color.black)
+                .clipShape(Circle())
+        }
+    }
+}
+
+struct OnboardingNextButton_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingNextButton()
+    }
+}
--- Hover/views/screen/onboarding/LoveLanguagePrompt.swift
+++ Hover/views/screen/onboarding/LoveLanguagePrompt.swift
@@ -9,29 +9,70 @@ import SwiftUI
 
 struct LoveLanguagePrompt: View {
     @Binding var onboardingStep: Int
+    @State var isNavigationActive: Bool = false
+    @State var isGotoTest: Bool = false
+    
+    let loveLanguages = ["Words of Affirmation", "Receiving Gifts", "Physical Touch", "Act of Service", "Quality Time"]
     
     var body: some View {
         VStack {
-            Text("Love Language Prompt")
-            Text("\(onboardingStep)")
-            if onboardingStep == 3 {
-                NavigationLink {
-                    LoveLanguagePrompt(onboardingStep: .constant(onboardingStep + 1))
-                } label: {
-                    Image(systemName: "chevron.forward")
+            Spacer()
+            HStack {
+                Text("What's \(onboardingStep == 3 ? "your": "your partner")\nLove Language?")
+                    .font(.title)
+                    .bold()
+                Spacer()
+            }
+            HStack {
+                Text("We need to know \(onboardingStep == 3 ? "your": "your partner") love language so we can recommend some activities to you.")
+                    .font(.subheadline)
+                    .foregroundColor(Color("CaptionColor"))
+                Spacer()
+            }
+            NavigationLink(isActive: $isNavigationActive) {
+                LoveLanguagePrompt(onboardingStep: .constant(onboardingStep + 1))
+            } label: {
+            }
+            ForEach(loveLanguages, id: \.self) { loveLanguage in
+                HStack {
+                    Text(loveLanguage)
+                    Spacer()
                 }
-            } else {
-                Button {
+                .padding()
+                .frame(height: 44)
+                .background(.white)
+                .cornerRadius(5)
+                .shadow(color: .black, radius: 1)
+            }
+            //            Spacer().frame(height: 30)
+            Button {
+                if self.onboardingStep == 3 {
+                    self.isNavigationActive.toggle()
+                } else {
                     withAnimation {
                         UserDefaults.standard.set(false, forKey: "isNewUser")
                     }
-                } label: {
-                    Image(systemName: "chevron.forward")
                 }
-
+            } label: {
+                OnboardingNextButton()
             }
+            Spacer()
+                .frame(height: 20)
+            Text("Don't know \(self.onboardingStep == 4 ? "your": "your partner") Love Language yet?")
+                .font(.subheadline)
+                .foregroundColor(Color("CaptionColor"))
+            NavigationLink {
+                QuestionLoveLanguage()
+            } label: {
+                Text("Take the test now")
+            }
+            Spacer().frame(height: 25)
         }
+        .padding()
+        //        .frame(height: UIScreen.main.bounds.height)
+        .background(Color("BackgroundColor")).ignoresSafeArea()
     }
+    
 }
 
 struct LoveLanguagePrompt_Previews: PreviewProvider {
