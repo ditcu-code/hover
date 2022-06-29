fd37eaea3ff76ca3f6625071766597fe2e1cbacc
ditcu-code <work.adityac@gmail.com>
2022-06-29 18:02:56 +0700

    ditcu - declare colors pallete

---
+++ Hover/services/Colors.swift
@@ -0,0 +1,29 @@
+//
+//  Colors.swift
+//  MC1-Group12
+//
+//  Created by Aditya Cahyo on 14/05/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let darkGrey = Color("DarkGrey")
+    static let redPastel = Color("RedPastel")
+    static let yellowCorn = Color("YellowCorn")
+    static let yellowSun = Color("YellowSun")
+    
+    static let actOfService = Color("ActOfService")
+    static let physicalTouch = Color("PhysicalTouch")
+    static let qualityTime = Color("QualityTime")
+    static let receivingGift = Color("ReceivingGift")
+    static let combination = Color("Combination")
+    static let wordsOfAffirmation = Color("WordsOfAffirmation")
+    
+    static let bgActOfService = Color("ActOfServiceBg")
+    static let bgPhysicalTouch = Color("PhysicalTouchBg")
+    static let bgQualityTime = Color("QualityTimeBg")
+    static let bgReceivingGift = Color("ReceivingGiftBg")
+    static let bgCombination = Color("CombinationBg")
+    static let bgWordsOfAffirmation = Color("WordsOfAffirmationBg")
+}
