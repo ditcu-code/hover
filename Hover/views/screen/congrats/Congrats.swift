0d79c3b0e44f26e7444857740221a0cea532dd1f
ditcu-code <work.adityac@gmail.com>
2022-07-05 01:22:34 +0700

    add congrats screen

---
+++ Hover/views/screen/congrats/Congrats.swift
@@ -0,0 +1,32 @@
+//
+//  Congrats.swift
+//  Hover
+//
+//  Created by Aditya Cahyo on 05/07/22.
+//
+
+import SwiftUI
+
+struct Congrats: View {
+    var body: some View {
+        ZStack {
+            Color.backgroundColor.ignoresSafeArea()
+            VStack {
+                Image("congrats").resizable().aspectRatio(contentMode: .fit)
+                VStack(alignment: .leading) {
+                    Text("Nice Job, Jeniver!").font(.title).bold()
+                    Text("You have succeeded in making a special day plan to get closer to your partner.\n\nDon't forget to prepare yourself for the special day ✨")
+                }
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 8).fill(.white))
+                
+            }.padding()
+        }
+    }
+}
+
+struct Congrats_Previews: PreviewProvider {
+    static var previews: some View {
+        Congrats()
+    }
+}
