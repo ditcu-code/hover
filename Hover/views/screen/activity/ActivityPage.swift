7912e0beea29baca910010fd286a6058c43d96e9
m-aleandroo13 <aleandro1312@gmail.com>
2022-07-05 04:10:25 +0700

    activity

--- Hover/views/screen/activity/ActivityPage.swift
+++ Hover/views/screen/activity/ActivityPage.swift
@@ -19,12 +19,17 @@ struct ActivityPage: View {
     @State private var selectedActivity3: [(ActivityList,LoveLanguages)] = []
     @State private var listActivities : [[(ActivityList,LoveLanguages)]] = [[]]
     @State private var randomElement: [(ActivityList,LoveLanguages)] = []
-//    [
-//      [(ActivityList, LoveLanguages)],
-//      [(ActivityList, LoveLanguages)]
-//    ]
-//    [(ActivityList, LoveLanguages)]
-//    @Binding var partner: User
+    @State private var filteredActivities: [ActivityList] = []
+    @State private var randomActivities: [ActivityList] = []
+    //    [
+    //      (ActivityList, LoveLanguages) ?
+    //        ActivityList -> [LoveLanguages]
+    //      let activity = ActivityList
+    //      loveLanguages = activity.llArray
+    //      for i in loveLanguages { }
+    //    ]
+    //    [(ActivityList, LoveLanguages)]
+    //    @Binding var partner: User
     var loveLanguageUser: LoveLanguageUser {
         LoveLanguageUser(user: globalObject.user)
     }
@@ -32,146 +37,101 @@ struct ActivityPage: View {
         LoveLanguageUser(user: globalObject.partner)
     }
     var body: some View {
-//        NavigationView {
-            ZStack(alignment:.topLeading) {
-                Rectangle().fill(.clear)
-                Image("bg-home").resizable().aspectRatio(contentMode: .fit).ignoresSafeArea()
+        //        NavigationView {
+        ZStack(alignment:.topLeading) {
+            Rectangle().fill(.clear)
+            Image("bg-home").resizable().aspectRatio(contentMode: .fit).ignoresSafeArea()
+            
+            
+            VStack(alignment: .leading) {
+                Text("Have you done this together 👩‍❤️‍👨 \nwith \(globalObject.partner.wrappedName)?").font(.headline).padding(.horizontal)
                 
-               
-                VStack(alignment: .leading) {
-                    Text("Have you done this together 👩‍❤️‍👨 \nwith \(globalObject.partner.wrappedName)?").font(.headline).padding(.horizontal)
-                    
-                    HStack {
-                    IconLL()
-                        ZStack(alignment: .topLeading) {
-                            RoundedCorner(radius: 8, corners: [.topLeft, .bottomLeft]).fill(.black).opacity(0.5)
-                            VStack(alignment: .leading) {
-                                if !selectedActivity.isEmpty {
-                                    Text(selectedActivity[0].0.wrappedActivity).font(.title3).bold().shadow(color: .black, radius: 2, x: 1, y: 1)
-                                        Spacer()
-                                    HStack {
-                                        Spacer()
-                                        ForEach(selectedActivity[0].0.llArray) { ll in
-                                            Text(ll.wrappedLLName).font(.subheadline)
-                                        }
+                HStack {
+                    if !filteredActivities.isEmpty {
+                        LoveLanguageLogoBg(loveLanguageName: getLLLogo(llData: filteredActivities[0].llArray) , size: 45, cornerRadius: 8)
+                    }
+                    ZStack(alignment: .topLeading) {
+                        RoundedCorner(radius: 8, corners: [.topLeft, .bottomLeft]).fill(.black).opacity(0.5)
+                        VStack(alignment: .leading) {
+                            if !filteredActivities.isEmpty {
+                                Text(filteredActivities[0].wrappedActivity).font(.title3).bold().shadow(color: .black, radius: 2, x: 1, y: 1)
+                                Spacer()
+                                HStack {
+                                    Spacer()
+                                    ForEach(filteredActivities[0].llArray) { ll in
+                                        Text(ll.wrappedLLName).font(.subheadline)
                                     }
                                 }
-                            }.padding()
-                        }
-                        .frame(height: 120)
-                        .foregroundColor(.white)
-                    }.padding(.leading)
-                    Text("Have you tried these activities to \(globalObject.partner.wrappedName)? \nHe definitely will happy 🤩").font(.headline).padding()
-                        .lineLimit(nil)
+                            }
+                            
+                        }.padding()
+                    }
+                    .frame(height: 120)
+                    .foregroundColor(.white)
+                }.padding(.leading)
+                Text("Have you tried these activities to \(globalObject.partner.wrappedName)? \nHe definitely will happy 🤩").font(.headline).padding()
+                    .lineLimit(nil)
+                ForEach(randomActivities, id: \.self) { act in
                     HStack {
-                        IconLL()
+                        LoveLanguageLogoBg(loveLanguageName: getLLLogo(llData: act.llArray) , size: 45, cornerRadius: 8)
                         ZStack {
-                            if !selectedActivity1.isEmpty {
-                                Image("bg-act-wordsofaffirmation").resizable().aspectRatio(contentMode: .fit)
-                                
-                                VStack(alignment: .leading) {
-                                    Text(selectedActivity1[0].0.wrappedActivity).font(.title3).bold().padding(.leading, 15)
+                            getLoveLanguageBg(loveLanguage: getLLLogo(llData: act.llArray))
+                            VStack(alignment: .leading) {
+                                Text(act.wrappedActivity)
+                                    .font(.title3)
+                                    .bold()
+                                    .padding(.leading, 15)
+                                Spacer()
+                                HStack {
                                     Spacer()
-                                    HStack {
-                                        Spacer()
-                                        ForEach(selectedActivity1[0].0.llArray) { ll in
-                                            Text(ll.wrappedLLName).font(.subheadline)
-                                        }
+                                    ForEach(act.llArray, id: \.self) { ll in
+                                        Text(ll.wrappedLLName).font(.subheadline)
                                     }
-                                }.padding(10)
+                                }
                             }
                         }
-                    }.padding(.leading).frame(height: 120)
-                    HStack {
-                        IconLL()
-                        ZStack {
-                            if !selectedActivity2.isEmpty {
-                                Image("bg-act-wordsofaffirmation").resizable().aspectRatio(contentMode: .fit)
-                                
-                                VStack(alignment: .leading) {
-                                    Text(selectedActivity2[0].0.wrappedActivity).font(.title3).bold().padding(.leading, 15)
-                                    Spacer()
-                                    HStack {
-                                        Spacer()
-                                        ForEach(selectedActivity2[0].0.llArray) { ll in
-                                            Text(ll.wrappedLLName).font(.subheadline)
-                                        }
-                                    }
-                                }.padding(10)
-                            }
-                        }
-                    }.padding(.leading).frame(height: 120)
-                    HStack {
-                        IconLL()
-                        ZStack {
-                            if !selectedActivity3.isEmpty {
-                                Image("bg-act-wordsofaffirmation").resizable().aspectRatio(contentMode: .fit)
-                                
-                                VStack(alignment: .leading) {
-                                    Text(selectedActivity3[0].0.wrappedActivity).font(.title3).bold().padding(.leading, 15)
-                                    Spacer()
-                                    HStack {
-                                        Spacer()
-                                        ForEach(selectedActivity3[0].0.llArray) { ll in
-                                            Text(ll.wrappedLLName).font(.subheadline)
-                                        }
-                                    }
-                                }.padding(10)
-                            }
-                        }
-                    }.padding(.leading).frame(height: 120)
-                    
-                }.border(.red)
-                
-                .navigationTitle("Activities")
+                    }
+                }
             }
-            .onAppear{
-                getActivity()
-//                print(listActivities)
-//                print("=========================")
-//                randomize()
-                self.selectedActivity = self.listActivities.randomElement()!
-                self.selectedActivity1 = self.listActivities.randomElement()!
-                self.selectedActivity2 = self.listActivities.randomElement()!
-                self.selectedActivity3 = self.listActivities.randomElement()!
-                
-//                print(selectedActivity[0].0)
-//                print(selectedActivity[0].0.llArray[0].)
-            }
-//        }
+            
+            .navigationTitle("Activities")
+        }
+        .onAppear{
+            getActivity()
+            randomizeActivity()
+        }
     }
+    
+    func randomizeActivity() {
+        randomActivities = []
+        while randomActivities.count < 3 {
+            let random = filteredActivities.randomElement()!
+            let isExist = randomActivities.filter { activity in
+                activity.wrappedActivity == random.wrappedActivity || random.wrappedActivity == filteredActivities[0].wrappedActivity
+            }
+            if isExist.count == 0 {
+                randomActivities.append(random)
+            }
+        }
+    }
+    
     func getActivity(){
         let userPrimaryLL = getPrimaryLoveLanguageUser()
         let partnerPrimaryLL = getPrimaryLoveLanguagePartner()
         let partnerSecondaryLL = getSecondaryLoveLanguagePartner()
-        let selectedLL = loveLanguages.filter { ll in
-            ll.wrappedLLName == userPrimaryLL || ll.wrappedLLName == partnerPrimaryLL || ll.wrappedLLName == partnerSecondaryLL
+        
+        let filteredActivites = activities.filter { activity in
+            return activity.llArray.filter{ loveLanguage in
+                loveLanguage.wrappedLLName == userPrimaryLL || loveLanguage.wrappedLLName == partnerPrimaryLL || loveLanguage.wrappedLLName == partnerSecondaryLL
+            }.count > 0
         }
         
-        for activity in activities {
-            for loveLanguage in selectedLL {
-                    if !loveLanguage.wrappedLLName.isEmpty && !activity.wrappedActivity.isEmpty{
-                        listActivities.append([(activity,loveLanguage)])
-                }
-            }
+        let sortBasedOnLL = filteredActivites.sorted { a, b in
+            a.llArray.count > b.llArray.count
         }
+        
+        self.filteredActivities = sortBasedOnLL
     }
-//    func randomize(){
-////        var random : (ActivityList,LoveLanguages) =  (ActivityList(), LoveLanguages())
-//        for _ in 0...2{
-//            let randomIndex = Int.random(in: 0..<listActivities.count-1)
-//            print(listActivities[randomIndex].count)
-//            print(listActivities[randomIndex])
-//            for i in 0...listActivities[randomIndex].count-1{
-//                print(listActivities[randomIndex][i])
-//                selectedActivity.append(listActivities[randomIndex][i])
-//            }
-//
-////                selectedActivity.append(listActivities[randomIndex])
-////            }
-//        }
-//
-//    }
     func getPrimaryLoveLanguageUser() -> String {
         return loveLanguageUser.getPrimaryLoveLanguage()
     }
@@ -186,6 +146,8 @@ struct ActivityPage: View {
 struct ActivityPage_Previews: PreviewProvider {
     static var previews: some View {
         ActivityPage()
+            .environmentObject(GlobalObject.shared)
+            .environment(\.managedObjectContext, CoreDataPreviewHelper.preview.container.viewContext)
     }
 }
 
@@ -196,10 +158,10 @@ extension View {
 }
 
 struct RoundedCorner: Shape {
-
+    
     var radius: CGFloat = .infinity
     var corners: UIRectCorner = .allCorners
-
+    
     func path(in rect: CGRect) -> Path {
         let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
         return Path(path.cgPath)
@@ -207,10 +169,14 @@ struct RoundedCorner: Shape {
 }
 
 struct IconLL: View {
+    var loveLanguage: [LoveLanguages]
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 8).fill(Color.bgCombination).frame(width: 45, height: 45)
-            Image(systemName: "heart.fill").foregroundColor(.combination).font(.title2)
+            
+            LoveLanguageLogoBg(loveLanguageName: getLLLogo(llData: loveLanguage) , size: 45, cornerRadius: 8)
+            
         }
     }
 }
+
